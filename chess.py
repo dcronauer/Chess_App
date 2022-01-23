c1e3b66637568edd26efa2c984195281b0aaf13c
Daniel Cronauer <dcronaue@gmail.com>
2022-01-23 16:15:59 -0500

    got board and pieces and positioning working, now have to figure out why images are replacing each other

--- chess.py
+++ chess.py
@@ -1,6 +1,7 @@
 
-import sys
 from turtle import position
+import chessboard
+import sys
 #import pygame
 DICT_COLUMNS = {'A': 1,'B': 2,'C': 3,'D': 4,'E': 5,'F': 6,'G': 7, 'H': 8}
 DICT_ROWS = {1: 1,2: 2,3: 3,4: 4,5: 5,6: 6,7: 7,8: 8}
@@ -8,8 +9,8 @@ DICT_COLUMNS_REVERSED = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G',
 DICT_POSSIBLE_MOVES = {}
 DICT_PIECES = {}
 DICT_PIECE_REVERSE = {}
-PIECE_DICTIONARY = {'wp': ['white', 'pawn', 'path',8],  'wn': ['white', 'knight','path',2], 'wb': ['white', 'bishop','path',2], 'wr': ['white', 'rook','path',2], 'wq': ['white', 'queen','path',1], 'wk': ['white', 'king','path',1], \
-    'bp': ['black','pawn','path',8], 'bn': ['black', 'knight','path',2], 'bb': ['black', 'bishop','path',2], 'br': ['black', 'rook','path',2], 'bq': ['black', 'queen','path',1], 'bk': ['black', 'king','path',1]}
+PIECE_DICTIONARY = {'wp': ['white', 'pawn', 'C://Users/Locke/projects/Chess_App/images/white_p.png',8],  'wn': ['white', 'knight','images/white_n.png',2], 'wb': ['white', 'bishop','images/white_b.png',2], 'wr': ['white', 'rook','images/white_r.png',2], 'wq': ['white', 'queen','images/white_q.png',1], 'wk': ['white', 'king','images/white_k.png',1], \
+    'bp': ['black','pawn','images/black_p.png',8], 'bn': ['black', 'knight','images/black_n.png',2], 'bb': ['black', 'bishop','images/black_b.png',2], 'br': ['black', 'rook','images/black_r.png',2], 'bq': ['black', 'queen','images/black_q.png',1], 'bk': ['black', 'king','images/black_k.png',1]}
 BOARD_CORDINATES = {'A1': None, 'A2': None, 'A3': None , 'A4': None, 'A5': None, 'A6': None, 'A7': None, 'A8': None, 'B1': None, 'B2': None , 'B3': None, 'B4': None, 'B5': None, 'B6': None, 'B7': None, 'B8': None, 'C1': None, 'C2': None, 
  'C3': None, 'C4': None, 'C5': None, 'C6': None, 'C7': None, 'C8': None, 'D1': None, 'D2': None, 'D3': None, 'D4': None, 'D5': None, 'D6': None, 'D7': None, 'D8': None, 'E1': None, 'E2': None, 'E3': None, 'E4': None, 'E5': None, 'E6': None, 
  'E7': None, 'E8': None, 'F1': None, 'F2': None, 'F3': None, 'F4': None, 'F5': None, 'F6': None, 'F7': None, 'F8': None, 'G1': None, 'G2': None, 'G3': None, 'G4': None, 'G5': None, 'G6': None, 'G7': None, 'G8': None, 'H1': None, 'H2': None, 
@@ -20,9 +21,10 @@ BOARD_CORDINATES = {'A1': None, 'A2': None, 'A3': None , 'A4': None, 'A5': None,
 #  'H3': None, 'H4': None, 'H5': None, 'H6': None, 'H7': 'bp8', 'H8': 'br2'}
 POSITION_START = {'wr1': 'A1', 'wp1': 'A2', 'bp1': 'A7', 'br1': 'A8', 'wn1': 'B1', 'wp2': 'B2', 'bp2': 'B7', 'bn1': 'B8', 'wb1': 'C1', 'wp3': 'C2', 'bp3': 'C7', 'bb1': 'C8', 'wq1': 'D1', 'wp4': 'D2', 'bp4': 'D7', 'bq1': 'D8', 
 'wk1': 'E1', 'wp5': 'E2', 'bp5': 'E7', 'bk1': 'E8', 'wb2': 'F1', 'wp6': 'F2', 'bp6': 'F7', 'bb2': 'F8', 'wn2': 'G1', 'wp7': 'G2', 'bp7': 'G7', 'bn2': 'G8', 'wr2': 'H1', 'wp8': 'H2', 'bp8': 'H7', 'br2': 'H8'}
+POSITION_NW = {'A1': [0, 700], 'A2': [0, 600], 'A3': [0, 500], 'A4': [0, 400], 'A5': [0, 300], 'A6': [0, 200], 'A7': [0, 100], 'A8': [0, 0], 'B1': [100, 700], 'B2': [100, 600], 'B3': [100, 500], 'B4': [100, 400], 'B5': [100, 300], 'B6': [100, 200], 'B7': [100, 100], 'B8': [100, 0], 'C1': [200, 700], 'C2': [200, 600], 'C3': [200, 500], 'C4': [200, 400], 'C5': [200, 300], 'C6': [200, 200], 'C7': [200, 100], 'C8': [200, 0], 'D1': [300, 700], 'D2': [300, 600], 'D3': [300, 500], 'D4': [300, 400], 'D5': [300, 300], 'D6': [300, 200], 'D7': [300, 100], 'D8': [300, 0], 'E1': [400, 700], 'E2': [400, 600], 'E3': [400, 500], 'E4': [400, 400], 'E5': [400, 300], 'E6': [400, 200], 'E7': [400, 100], 'E8': [400, 0], 'F1': [500, 700], 'F2': [500, 600], 'F3': [500, 500], 'F4': [500, 400], 'F5': [500, 300], 'F6': [500, 200], 'F7': [500, 100], 'F8': [500, 0], 'G1': [600, 700], 'G2': [600, 600], 'G3': [600, 500], 'G4': [600, 400], 'G5': [600, 300], 'G6': [600, 200], 'G7': [600, 100], 'G8': [600, 0], 'H1': [700, 700], 'H2': [700, 600], 'H3': [700, 500], 'H4': [700, 400], 'H5': [700, 300], 'H6': [700, 200], 'H7': [700, 100], 'H8': [700, 0]}
+
 class Pieces:
-   
-    
+
     def __init__(self, color, type, image, position = None, destroyed = False, kill = False, moved = False):
         self.color = color
         self.type = type
@@ -34,10 +36,15 @@ class Pieces:
     #accessor
     def get_piece(self):
         return (self.color,self.type,self.image, self.position)
-    def set_position(self, item,key):
-        piece = Pieces.get_piece(item)
-        self.position = POSITION_START[key]
+    def set_position(self):
+        
+        block = DICT_PIECE_REVERSE[self]
+         
+        self.position = POSITION_START[block]
+       
+
         BOARD_CORDINATES[self.position] = self
+        
     def rook_moves(self,position):
         max_range = 8
         row = int(position[1])
@@ -58,14 +65,6 @@ class Pieces:
         BOARD_CORDINATES[self.position]
         self.position = None
         self.destroyed = True
-
-        
-
-           
-        
-        
-
-
     
 def above(starting_row, column, max_range,position):
     while max_range > starting_row:
@@ -86,16 +85,19 @@ def right(starting_row, starting_column, max_range, position):
         column = DICT_COLUMNS_REVERSED[starting_column]
         DICT_POSSIBLE_MOVES[column +str(starting_row)] = column+str(starting_row)
 
-def create_pieces():
-    total = 0
-    for item in PIECE_DICTIONARY:
-        list = PIECE_DICTIONARY[item]
-        for i in range(list[3]):
-            piece = Pieces(list[0],list[1],list[2])
+# def create_pieces():
+#     total = 0
+#     for item in PIECE_DICTIONARY:
+#         list = PIECE_DICTIONARY[item]
+#         for i in range(list[3]):
+#             piece = Pieces(list[0],list[1],list[2])
             
-            total += 1
-            DICT_PIECES[item+str(i+1)] = piece
-            DICT_PIECE_REVERSE[piece] = item + str(i+1)
+#             total += 1
+#             DICT_PIECES[item+str(i+1)] = piece
+#             DICT_PIECE_REVERSE[piece] = item + str(i+1)
+#             img = chessboard.tk.PhotoImage(file=list[2])
+#             chessboard.Chess_GUI.board.create_image(100,100, image=img)
+            
 # def create_board_cordinates():
 #     letters =['A','B','C','D','E','F','G','H']    
 #     for letter in letters:
@@ -119,19 +121,28 @@ def populate_board():
                 
        
 
+
 def main():
-    create_pieces()
-    populate_board()
-    print(DICT_PIECES)
-    print(DICT_PIECE_REVERSE)
-    position = POSITION_START['br1']
-    rook = DICT_PIECES["br1"]
-    result = rook.get_piece()
-    print(result)  
-    rook.rook_moves(position)
-    result = rook.get_piece()
-    print(result)  
-    #kill_piece(rook)
+    #  print('here')
+    #  letters =['A','B','C','D','E','F','G','H'] 
+    #  dict_test = {}
+    #  x = 0
+    #  y = 700
+    #  for letter in letters:
+    #     for i in range(1,9):
+    #         dict_test[letter + str(i)] = [x,y]
+    #         y -= 100
+    #     y = 700
+    #     x += 100
+    #  print(dict_test)
+     instance = chessboard.Chess_GUI()  
+        
+     chessboard.tkinter.mainloop()
+     
+
+        
+
+
 if __name__ == "__main__":    
     main()
         
--- chessboard.py
+++ chessboard.py
@@ -1,6 +1,7 @@
 from chess import *
+from PIL import ImageTk, Image
 import tkinter
-from tkinter import ttk
+from tkinter import Tk, ttk
 
 def main():
     print("wea are here")
@@ -20,6 +21,8 @@ class Chess_GUI:
         self.main_window.title('Chess App')
         self.main_window.minsize(800,800)
         self.create_canvas()
+        self.create_pieces()
+        
         
     def create_canvas(self):
         x = 0
@@ -28,31 +31,40 @@ class Chess_GUI:
         height = 100
         self.main_window.grid_columnconfigure(0,weight=1)
         self.main_window.grid_rowconfigure(0,weight=1)
+        self.main_window.grid_rowconfigure(1,weight=1)
+
         self.board_frame = ttk.Frame(self.main_window)
         self.board_frame.grid(row=0,column=0,sticky="nsew")
        
         self.board_frame.grid_rowconfigure(0,weight=1)
         self.board_frame.grid_columnconfigure(0,weight=1)
         
+        self.frame2 = ttk.Frame(self.main_window)
+        self.frame2.grid(row=1,column=0, sticky="nsew")
+        self.frame2.grid_rowconfigure(1,weight=1)
+        self.frame2.grid_columnconfigure(0,weight=1)
+        
+        
         #self.rect = self.board.Canvas.create_rectangle(0,0,100,100,fill="red")
         self.board = tkinter.Canvas(self.main_window, bg = "black",width=800,height=800)
         self.board.grid(column=0,row=0, sticky="nsew")
+        
         
        
         for i in self.array_rows:
             
             if i % 2 != 0:
-                print(i,x,y,height,width)
+                #print(i,x,y,height,width)
                 x,y, height,width = self.draw_squares(x,y,height,width)
                 
-                print(i,x,y,height,width)
+                #print(i,x,y,height,width)
             elif i % 2 == 0:
                 
                 x = 100
                 width = 200
-                print(i,x,y,height,width)
+                #print(i,x,y,height,width)
                 x,y, height,width = self.draw_squares(x,y,height,width)
-                print(i,x,y,height,width)
+                #print(i,x,y,height,width)
                 
     def draw_squares(self,x,y,height,width):
         
@@ -67,7 +79,43 @@ class Chess_GUI:
         y += 100
         height += 100
         return x, y, height, width   
-          
+ 
+    def create_pieces(self):
+        
+                
+        total = 0
+        for item in PIECE_DICTIONARY:
+            list = PIECE_DICTIONARY[item]
+            print(list)
+            for i in range(list[3]):
+                piece = Pieces(list[0],list[1],list[2])
+                
+                
+                
+                
+                DICT_PIECES[item+str(i+1)] = piece
+                DICT_PIECE_REVERSE[piece] = item + str(i+1)
+                piece.set_position()
+                self.image = Image.open(list[2])
+                self.piece_image = ImageTk.PhotoImage(self.image)
+                piece_identity = DICT_PIECE_REVERSE[piece]
+                square = POSITION_START[piece_identity]
+                list1 = POSITION_NW[square]
+                x = list1[0]
+                y = list1[1]
+                print(x,y)
+
+                # self.label = ttk.Label(self.frame2,image = self.piece_image)
+                # self.label.grid(row = total, column=0)
+                self.canvas_piece = self.board.create_image(x,y,anchor="nw",image=self.piece_image)
+                
+                
+                total += 1
+        print(total)
+        print(BOARD_CORDINATES)
+
+
+        
 
        
              
